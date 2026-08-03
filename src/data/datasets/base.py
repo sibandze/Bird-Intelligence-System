076877376bd85340d21a8d2b3152d527115337a4
N Sibandze <56678793+sibandze@users.noreply.github.com>
2026-08-03 19:59:00 +0200

    Refactor dataset to include sampling strategy

--- src/data/datasets/base.py
+++ src/data/datasets/base.py
@@ -5,8 +5,8 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
-from .process_audio import load_local_spectrogram
-
+from ..process_audio import load_local_spectrogram
+from src.data.windowing import build_sampling_strategy, BaseSamplingStrategy
 
 class BaseSpectrogramDataset(Dataset):
     """
@@ -19,12 +19,14 @@ class BaseSpectrogramDataset(Dataset):
                  min_db: float,
                  max_db: float,
                  train: bool = True,
-                 spec_aug_config: dict = None):
+                 spec_aug_config: dict = None,
+                 sampling_config: dict = None,):
         self.df = df.reset_index(drop=True)
         self.segment_size = segment_size
         self.min_db = min_db
         self.max_db = max_db
         self.train = train
+        self.epoch = 0 
         self.spec_aug_config = spec_aug_config or {
             "enabled": True,
             "prob": 0.5,
@@ -33,9 +35,15 @@ class BaseSpectrogramDataset(Dataset):
             "num_time_masks": 2,
             "time_mask_param": 10
         }
+        self.sampling_strategy: BaseSamplingStrategy = build_sampling_strategy(sampling_config)
+        
 
     def __len__(self) -> int:
         return len(self.df)
+
+    def set_epoch(self, epoch: int):
+        """Allows PyTorch Lightning / Trainer loop to update current epoch."""
+        self.epoch = epoch
 
     def _load_and_preprocess(self, path: str) -> torch.Tensor:
         """Loads, normalizes, crops/pads a spectrogram into a 3D Tensor [1, F, T]."""
@@ -51,7 +59,12 @@ class BaseSpectrogramDataset(Dataset):
     def _crop_or_pad(self, mel: np.ndarray) -> np.ndarray:
         T = mel.shape[1]
         if T > self.segment_size:
-            start = random.randint(0, T - self.segment_size) if self.train else (T - self.segment_size) // 2
+            start = self.sampling_strategy.get_start_frame(
+                total_frames=T,
+                segment_size=self.segment_size,
+                epoch=self.epoch,
+                is_train=self.train
+            )
             return mel[:, start:start + self.segment_size]
         else:
             pad = self.segment_size - T
