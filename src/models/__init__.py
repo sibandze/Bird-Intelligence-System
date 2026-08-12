28c3d04d4a87fef4dcf423a4499b374b9d694056
N Sibandze <56678793+sibandze@users.noreply.github.com>
2026-08-12 12:36:00 +0200

    Import SupervisedTransformer in models init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,7 +4,7 @@ Model definitions package for Bird-Intelligence-System.
 Contains model architectures and related utilities.
 """
 
-from .supervised_transformer import 
+from .supervised_transformer import SupervisedTransformer
 from .ssl.simclr import SimCLR
 
 __all__ = ['SupervisedTransformer', 'SimCLR']
