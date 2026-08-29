df5671979c0a7549402866bc45a7ab67348accb6
N Sibandze <56678793+sibandze@users.noreply.github.com>
2026-08-29 12:55:10 +0200

    Update docstring for utils package

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,6 @@
-"""Utility helpers package for Bird-Intelligence-System.
+# src/utils/__init__.py
+"""
+Utility helpers package for Bird-Intelligence-System.
 
 General-purpose utility functions.
 """
