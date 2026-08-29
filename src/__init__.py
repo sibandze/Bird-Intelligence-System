b2f2e3a67543eea76a246d3329fc831e6fac8810
N Sibandze <56678793+sibandze@users.noreply.github.com>
2026-08-29 12:56:21 +0200

    Update docstring in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
-"""Top-level src package for Bird-Intelligence-System.
+# src/__init__.py
+"""
+Top-level src package for Bird-Intelligence-System.
 
 This file makes the `src` directory a Python package.
 """
